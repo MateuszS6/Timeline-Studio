0025ec342493d8082fa9301e61c266b86c30e0c7
Mateusz Stepien <124052017+MateuszS6@users.noreply.github.com>
2026-08-26 16:44:03 +0100

    Display project headers horizontally

--- src/components/TimelineGrid.tsx
+++ src/components/TimelineGrid.tsx
@@ -1,7 +1,30 @@
-export default function TimelineGrid(){
-    return(
+import { useEffect, useState } from "react"
+import type { Project } from "../types/project"
+import { getProjects } from "../services/projects";
+
+export default function TimelineGrid() {
+    const [projects, setProjects] = useState<Project[]>([]);
+
+    useEffect(() => {
+        async function load() {
+            setProjects(await getProjects());
+        }
+
+        load();
+    })
+
+    return (
         <main className="timeline">
-            Timeline
+            <div className="project-header">
+                {projects.map(project =>
+                    <div
+                        key={project.id}
+                        className="project-column"
+                    >
+                        {project.title}
+                    </div>
+                )}
+            </div>
         </main>
     )
 }
